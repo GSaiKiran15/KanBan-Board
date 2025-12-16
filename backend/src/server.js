0eb6c0b2c2b04f0c17b90272efd62e0532841637
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2025-12-16 15:32:22 -0700

    testing the backend with a dummy app.get(/)

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,9 +32,9 @@ admin.initializeApp({
 const app = express();
 
 const allowedOrigins = [
-  process.env.FRONTEND_URL || "http://localhost:5173",
-  "http://localhost:5173",
-  "https://kanban-backend-ahbb.onrender.com/",
+  process.env.FRONTEND_URL ||
+    "http://localhost:5173" ||
+    "https://kanban-backend-ahbb.onrender.com/",
 ];
 
 app.use(
@@ -66,6 +66,10 @@ const authenticate = async (req, res, next) => {
     return res.status(401).json({ error: "Unauthorized - Invalid token" });
   }
 };
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 
 app.post("/api/newUser", async (req, res) => {
   const { uid, email, name } = req.body;
